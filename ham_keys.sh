8eaea3844ab7727bc6ee401ae1f18494d74ce491
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-08-15 16:03:46 +0330

    update ham_keys.sh

--- ham_keys.sh
+++ ham_keys.sh
@@ -61,6 +61,10 @@ games[4, name]="Train Miner"
 games[4, appToken]="82647f43-3f87-402d-88dd-09a90025313f"
 games[4, promoId]="c4480ac7-e178-4973-8061-9ed5b2e17954"
 
+games[5, name]="Merge Away"
+games[5, appToken]="8d1cc2ad-e097-4b86-90ef-7a27e19fb833"
+games[5, promoId]="dc128d28-c45b-411c-98ff-ac7726fbaea4"
+
 # Proxys
 load_proxies() {
 	if [[ -f "$1" ]]; then
@@ -194,7 +198,7 @@ main() {
 	load_proxies "$PROXY_FILE"
 
 	while true; do
-		for game_choice in {1..4}; do
+		for game_choice in {1..5}; do
 			if [[ ${#proxies[@]} -gt 0 ]]; then
 				proxy=${proxies[RANDOM % ${#proxies[@]}]}
 			else
