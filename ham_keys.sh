8d26da5f0a655a262795642ce5acb0169935b0b7
saberloty <sabertayyeb@gmail.com>
2024-08-18 15:41:10 +0330

    Update ham_keys.sh

--- ham_keys.sh
+++ ham_keys.sh
@@ -200,7 +200,7 @@ main() {
 	load_proxies "$PROXY_FILE"
 
 	while true; do
-		for game_choice in {1..6}; do
+		for game_choice in {5..6}; do
 			if [[ ${#proxies[@]} -gt 0 ]]; then
 				proxy=${proxies[RANDOM % ${#proxies[@]}]}
 			else
