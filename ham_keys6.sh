44c90958e5b9940f340eac2faa71048ca5de9904
saberloty <sabertayyeb@gmail.com>
2024-08-28 13:03:40 +0330

    Update ham_keys6.sh

--- ham_keys6.sh
+++ ham_keys6.sh
@@ -33,7 +33,7 @@ echo ""
 echo -en "${purple}[Optional] ${green}Enter Your telegram Bot token: ${rest}"
 read -r TELEGRAM_BOT_TOKEN
 echo -e "${purple}============================${rest}"
-echo -en "${purple}[Optional] ${green}Enter Your Telegram Channel ID [example: ${yellow}@saber_dj95${green}]: ${rest}"
+echo -en "${purple}[Optional] ${green}Enter Your Telegram Channel ID [example: ${yellow}@P_Tech2024${green}]: ${rest}"
 read -r TELEGRAM_CHANNEL_ID
 echo -e "${purple}============================${rest}"
 echo -e "${green}generating ... Keys will be saved in [${yellow}my_keys.txt${green}]..${rest}"
@@ -65,9 +65,21 @@ games[5, name]="Twerk Race 3D"
 games[5, appToken]="61308365-9d16-4040-8bb0-2f4a4c69074c"
 games[5, promoId]="61308365-9d16-4040-8bb0-2f4a4c69074c"
 
-games[6, name]="My Clone Army"
-games[6, appToken]="74ee0b5b-775e-4bee-974f-63e7f4d5bacb"
-games[6, promoId]="fe693b26-b342-4159-8808-15e3ff7f8767"
+games[6, name]="Polysphere"
+games[6, appToken]="2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71"
+games[6, promoId]="2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71"
+
+games[7, name]="Mow and Trim"
+games[7, appToken]="ef319a80-949a-492e-8ee0-424fb5fc20a6"
+games[7, promoId]="ef319a80-949a-492e-8ee0-424fb5fc20a6"
+
+games[8, name]="Mud Racing"
+games[8, appToken]="8814a785-97fb-4177-9193-ca4180ff9da8"
+games[8, promoId]="8814a785-97fb-4177-9193-ca4180ff9da8"
+
+games[9, name]="Cafe Dash"
+games[9, appToken]="bc0971b8-04df-4e72-8a3e-ec4dc663cd11"
+games[9, promoId]="bc0971b8-04df-4e72-8a3e-ec4dc663cd11"
 
 # Proxys
 load_proxies() {
@@ -200,7 +212,7 @@ main() {
 	load_proxies "$PROXY_FILE"
 
 	while true; do
-		for game_choice in {6..6}; do
+		for game_choice in {9..9}; do
 			if [[ ${#proxies[@]} -gt 0 ]]; then
 				proxy=${proxies[RANDOM % ${#proxies[@]}]}
 			else
